c61ec5a096c0169eca194f3f4767208fb4e20291
Karvendhan M <karvsmech@gmail.com>
2022-06-22 14:20:48 +0530

    POST requests

--- lists/tests.py
+++ lists/tests.py
@@ -35,6 +35,14 @@ class HomePageTest(TestCase):
     #     self.assertTrue(html.strip().endswith('</html>'))
     #     self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')
+    #     self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
